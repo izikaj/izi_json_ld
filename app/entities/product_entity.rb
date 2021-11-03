4abb16937bc114348a2c618abf3d98cafeb9c74b
Izik AJ <izikaj@gmail.com>
2021-11-03 10:53:43 +0200

    add more fields to product

--- app/entities/product_entity.rb
+++ app/entities/product_entity.rb
@@ -4,11 +4,29 @@ class ProductEntity < ApplicationEntity
   type 'Product'
 
   attribute :name, ::IziJsonLd::Types::String
-  attribute? :brand, ::IziJsonLd::Types::String.optional
+  attribute? :brand, (::IziJsonLd::Types::Strict::String | ::OrganizationEntity).optional
   attribute? :image, ::IziJsonLd::Types::String.optional
   attribute? :description, ::IziJsonLd::Types::String.optional
+  attribute? :sku, ::IziJsonLd::Types::String.optional
+  attribute? :gtin, ::IziJsonLd::Types::String.optional
+  attribute? :mpn, ::IziJsonLd::Types::String.optional
 
   attribute? :offers, OfferEntity.optional
   attribute? :aggregateRating, AggregateRatingEntity.optional
   attribute? :review, ::IziJsonLd::Types::OneOrMore[::ReviewEntity].optional
+
+  def dump_attributes
+    attributes.except(:brand).merge(
+      brand: safe_brand
+    )
+  end
+
+  private
+
+  def safe_brand
+    return if brand.blank?
+    return brand unless brand.is_a?(String)
+
+    ::OrganizationEntity.new(name: brand)
+  end
 end
